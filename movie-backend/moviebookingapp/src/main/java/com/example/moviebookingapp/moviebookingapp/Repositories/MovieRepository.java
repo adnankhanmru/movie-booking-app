42ed04ab62e41e4ccbcac77b73f39af7cb96e0d8
harshchoudhary981066 <harsh981066@gmail.com>
2024-07-09 16:26:29 +0530

    Backend updated

--- movie-backend/moviebookingapp/src/main/java/com/example/moviebookingapp/moviebookingapp/Repositories/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.moviebookingapp.moviebookingapp.Repositories;
-
-import com.example.moviebookingapp.moviebookingapp.Entities.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-    Movie findByMovieName(String movieName);
-}
